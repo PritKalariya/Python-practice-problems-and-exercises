bbdf5f752caaf4455d1570ba3e265d0d9b705767
Prit Kalariya <kalariyaprit2000@gmail.com>
2020-11-16 17:52:14 +0530

    The Basics - Processing User Input

---
+++ The Basics - Processing User Input/String Formatting and Uppercase.py
@@ -0,0 +1,7 @@
+# Implementing a function that gets a person's name as a parameter and returns a greeting.
+# Note that the first letter of the person's name should always be uppercase
+
+def greeting(name):
+    return (f"Hii {name}").title()
+
+print(greeting('prit'))
---
+++ The Basics - Processing User Input/String formatting.py
@@ -0,0 +1,6 @@
+# Implement a function that gets a person's name as parameter and greets the person with 'Hi Person'.
+
+def greeting(name):
+     return (f"hii {name}")
+
+print(greeting('Prit'))
